603ec54e22d55f760cb472d3fca7f8e56b96b861
Sam Knight <sammknight@protonmail.com>
2022-11-15 11:51:56 -0600

    removed overlays

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,14 +1,5 @@
 final: prev: {
   hyprland = prev.hyprland.override { nvidiaPatches = true; };
-
-  cryptsetup = prev.cryptsetup.overrideAttrs (_: {
-    doCheck = false;
-  });
-
-  girara = prev.girara.overrideAttrs (_: {
-    doCheck = false;
-  });
-
   mathematica = prev.mathematica.overrideAttrs (_: {
     postInstall = ''
       ln -s "$out/libexec/Mathematica/Executables/wolframscript" "$out/bin/wolframscript"
