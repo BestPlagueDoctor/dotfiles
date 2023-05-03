14b2a20f614f3584c0ff3ceca7448110041e2dd7
Sam Knight <sammknight@protonmail.com>
2023-05-03 12:18:23 -0500

    see ya later 391 share

--- hosts/navi/default.nix
+++ hosts/navi/default.nix
@@ -133,35 +133,6 @@
         }
       ];
     };
-
-   samba = {
-    enable = true;
-    securityType = "user";
-    extraConfig = ''
-      workgroup = WORKGROUP
-      server string = smbnix
-      netbios name = smbnix
-      security = user 
-      #use sendfile = yes
-      min protocol = LANMAN1
-      # note: localhost is the ipv6 localhost ::1
-      hosts allow = 192.168.0. 127.0.0.1 localhost
-      hosts deny = 0.0.0.0/0
-      guest account = nobody
-      map to guest = bad user
-    '';
-    shares = {
-      ece391_share = {
-        path = "/home/sam/files/code/ece391/ece391_share";
-        browseable = "yes";
-        "read only" = "no";
-        "guest ok" = "yes";
-        "create mask" = "0644";
-        "directory mask" = "0755";
-        "force user" = "sam";
-        };
-      };
-    };  
   };
 
   hardware = {
