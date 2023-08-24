0b9e2895d3d635e440ee3f571b353cb773118d1f
Sam Knight <sammknight@protonmail.com>
2023-08-24 16:55:48 -0500

    virt stuff + cuda

--- hosts/lithium/default.nix
+++ hosts/lithium/default.nix
@@ -146,11 +146,9 @@
       driSupport = true;
       driSupport32Bit = true;
       extraPackages = with pkgs; [
-        amdvlk
         rocm-opencl-icd
         rocm-opencl-runtime
       ];
-      extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
     };
 
     nvidia = {
