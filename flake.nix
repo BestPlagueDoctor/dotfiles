66ea8908cbfb41592121cd5d42ad568e9750e9f8
Sam Knight <ksam1337@gmail.com>
2025-06-09 19:00:28 -0400

    oink oink

--- flake.nix
+++ flake.nix
@@ -5,68 +5,29 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-unstable-small";
     nixos-hardware.url = "github:nixos/nixos-hardware";
-
     nixgl.url = "github:nix-community/nixGL";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
 
-    ssbm.url = "github:djanatyn/ssbm-nix";
-
-    deploy-rs = {
-      url = "github:serokell/deploy-rs";
-      inputs.nixpkgs.follows = "nixpkgs";
+    armeenm-dotfiles = {
+      url = "github:armeenm/dotfiles";
+      #inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nix-misc = {
-      url = "github:armeenm/nix-misc";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.3.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    emacs-overlay = {
-      url = "github:nix-community/emacs-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-stable.follows = "nixpkgs-stable";
-    };
-
-    ragenix = {
-      url = "github:yaxitech/ragenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    #mpd-mpris = {
-    #  url = "github:natsukagami/mpd-mpris";
+    #deploy-rs = {
+    #  url = "github:serokell/deploy-rs";
     #  inputs.nixpkgs.follows = "nixpkgs";
     #};
-
-    disko = {
-      url = "github:nix-community/disko/latest";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }: let
+  outputs = inputs@{ self, ... }: let
+    inherit ( inputs.armeenm-dotfiles.inputs ) nixpkgs;
     config = {
       allowUnfree = true;
       contentAddressedByDefault = false;
     };
 
-    overlays = [ inputs.emacs-overlay.overlays.default ];
+    overlays = [ inputs.armeenm-dotfiles.overlays.default ];
 
-    forAllSystems = f: nixpkgs.lib.genAttrs [
-      "x86_64-linux"
-      "aarch64-linux"
-    ] (system: f system (
-      import nixpkgs { inherit system config overlays; }
-    ));
+    forAllSystems = inputs.armeenm-dotfiles.lib.forAllSystems;
 
     root = ./.;
     user = {
@@ -76,49 +37,42 @@
     };
 
     baseModules = [
-#      inputs.ssbm.homeManagerModule
-      { _module.args = {inherit inputs root user; }; }
+      { _module.args = {
+          inherit root user;
+          inputs = inputs.armeenm-dotfiles.inputs;
+        }; }
       { nixpkgs = { inherit config overlays; }; }
     ];
 
-    #hmModules = baseModules ++ [
-    #  inputs.mpd-mpris.homeManagerModules.default
-    #];
     hmModules = baseModules;
 
     modules = hmModules ++ [
-      inputs.ssbm.nixosModule
       inputs.home-manager.nixosModules.home-manager
       inputs.lanzaboote.nixosModules.lanzaboote
       inputs.ragenix.nixosModules.default
-      inputs.disko.nixosModules.disko
       ./modules
     ];
 
   in {
     nixosConfigurations = {
       lithium = nixpkgs.lib.nixosSystem {
-        modules = modules ++ [
+        modules = baseModules ++ [
+          inputs.armeenm-dotfiles.nixosModules.nixosInteractive
           ./hosts/lithium
         ];
       };
 
       magi = nixpkgs.lib.nixosSystem {
-        modules = modules ++ [
+        modules = baseModules ++ [
+          inputs.armeenm-dotfiles.nixosModules.nixosBase
           ./hosts/magi
         ];
       };
 
       navi = nixpkgs.lib.nixosSystem {
-        modules = modules ++ [
+        modules = baseModules ++ [
+          inputs.armeenm-dotfiles.nixosModules.nixosInteractive
           ./hosts/navi
-        ];
-      };
-
-      wsl = nixpkgs.lib.nixosSystem {
-        modules = modules ++ [
-          inputs.nixos-wsl.nixosModules.default
-          ./hosts/wsl
         ];
       };
     };
@@ -179,21 +133,7 @@
       };
     };
 
-    devShells = forAllSystems (system: pkgs: with pkgs; {
-      default = mkShell {
-        packages = [
-          inputs.deploy-rs.packages.${system}.default
-          nix-output-monitor
-          nvd
-          openssl
-        ];
-
-        shellHook = ''
-          export PATH=$PWD/util:$PATH
-        '';
-      };
-    });
-
+    devShells = inputs.armeenm-dotfiles.devShells;
     checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) inputs.deploy-rs.lib;
   };
 } 
