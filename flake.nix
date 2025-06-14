a4b6a50e64d71efca7401d5a2aab90b622323ace
Sam Knight <ksam1337@gmail.com>
2025-06-14 17:01:06 +0000

    x220 rebirth v2

--- flake.nix
+++ flake.nix
@@ -12,10 +12,10 @@
       #inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    #deploy-rs = {
-    #  url = "github:serokell/deploy-rs";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
+    deploy-rs = {
+      url = "github:serokell/deploy-rs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ self, ... }: let
