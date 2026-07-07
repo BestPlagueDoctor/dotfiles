ee45bef0c912fc6ead34f87a73c39d7cdfb2526c
sam knight <ksam1337@gmail.com>
2026-07-07 23:31:09 +0000

    FIX: rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,9 +3,9 @@ let
   magi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ8fE/7jy6+Q5CM+6tymMyJjv2QAvLFs30SQ0Cqjdsij";
   navi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzWFxJHJyWrXN7sVCHc7UMaJAFyQ7DH1BUdEVvdT9ot";
   motherbrain = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICgGwnnwmT1NmmFXKVoq1N7Z4fqI6NRAhwNldjDz3wFZ";
-  systems = [ lithium magi motherbrain ];
+  systems = [ lithium magi navi motherbrain ];
 in {
-  "cloudflare-api-token.age".publicKeys = [ lithium magi ];
-  "wg0.age".publicKeys = [ lithium magi ];
+  "cloudflare-api-token.age".publicKeys = systems;
+  "wg0.age".publicKeys = systems;
   "gitlab-runner.age".publicKeys = [ motherbrain ];
 }
